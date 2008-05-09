3496330b0e1f620a26d8b51e9ef886d70033b98d
David Alber <alber.david@gmail.com>
2008-05-09 04:20:22 +0000

    Continued reorganizing code for packaging (part of ticket #11).

---
+++ Geneagrapher/setup.py
@@ -0,0 +1,20 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+setup(name="Geneagrapher",
+      version="0.2",
+      description="Generates mathematic genealogy graph files.",
+      long_description="""\
+The Geneagrapher generates Graphviz "dot" files of mathematical
+genealogies using data from the Math Genealogy Project's website.
+""",
+      author="David Alber",
+      author_email="alber.david@gmail.com",
+      url="http://www.davidalber.net/",
+      packages=find_packages(exclude='tests'),
+      #package_data={'': '*.xml'},
+      install_requires=[],
+      test_suite = "tests.tests"
+      )
+
