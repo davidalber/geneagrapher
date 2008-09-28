751b7b77a6aa3be389933aeec6c7d23874fe59cd
David Alber <alber.david@gmail.com>
2008-09-28 23:06:49 +0000

    Removed comment line.

--- Geneagrapher/setup.py
+++ Geneagrapher/setup.py
@@ -13,9 +13,8 @@ genealogies using data from the Math Genealogy Project's website.
       author_email="alber.david@gmail.com",
       url="http://www.davidalber.net/",
       packages=find_packages(exclude='tests'),
-      #package_data={'': '*.xml'},
       install_requires=[],
       scripts=['geneagrapher/ggrapher.py'],
       test_suite = "tests.tests"
-      )
+)
 
