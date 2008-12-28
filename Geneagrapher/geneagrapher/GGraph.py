a3558a2eca182353bf6966ea7d71bde951858d96
David Alber <alber.david@gmail.com>
2008-12-28 05:08:13 +0000

    Fix indentations in DuplicateNodeError class.

--- Geneagrapher/geneagrapher/GGraph.py
+++ Geneagrapher/geneagrapher/GGraph.py
@@ -3,8 +3,8 @@
 class DuplicateNodeError(Exception):
     def __init__(self, value):
         self.value = value
-        def __str__(self):
-            return repr(self.value)
+    def __str__(self):
+        return repr(self.value)
 
 class Record:
     """
