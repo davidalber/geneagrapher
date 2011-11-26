e289dd3db89060bd84b5a7bc8758c11e11fef98c
David Alber <alber.david@gmail.com>
2011-11-26 11:42:23 -0800

    Removed unnecessary code from tests.py.

--- tests/tests.py
+++ tests/tests.py
@@ -1,19 +1,5 @@
-import unittest
 from test_record_methods import TestRecordMethods
 from test_node_methods import TestNodeMethods
 from test_graph_methods import TestGraphMethods
 from test_grabber_methods import TestGrabberMethods
 from test_geneagrapher_methods import TestGeneagrapherMethods
-
-
-def runTests():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestRecordMethods))
-    suite.addTest(unittest.makeSuite(TestNodeMethods))
-    suite.addTest(unittest.makeSuite(TestGraphMethods))
-    suite.addTest(unittest.makeSuite(TestGrabberMethods))
-    suite.addTest(unittest.makeSuite(TestGeneagrapherMethods))
-    unittest.TextTestRunner(verbosity=1).run(suite)
-
-if __name__ == '__main__':
-    runTests()
