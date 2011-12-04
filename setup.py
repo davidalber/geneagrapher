556701eddefedafed3de42af413c9865cc7892ee
David Alber <alber.david@gmail.com>
2011-12-03 22:35:48 -0800

    Repackaging using Distribute (replacing setuptools).
    Adopting the package tree and packaging files produced by the
    modern-package-template package.
    
    This references #11.

--- setup.py
+++ setup.py
@@ -1,22 +1,37 @@
-from ez_setup import use_setuptools
-use_setuptools()
 from setuptools import setup, find_packages
+import sys, os
 
-setup(name="Geneagrapher",
-      version="0.2.1-r2",
-      description="Generates mathematic genealogy graph files.",
-      long_description="""\
-The Geneagrapher generates Graphviz "dot" files of mathematical
-genealogies using data from the Math Genealogy Project's website.
-""",
-      author="David Alber",
-      author_email="alber.david@gmail.com",
-      url="http://www.davidalber.net/",
-      license="MIT",
-      packages=find_packages(exclude='tests'),
-      install_requires=['BeautifulSoup >= 3.2.0'],
-      entry_points={
-        'console_scripts': ['ggrapher = geneagrapher.geneagrapher:ggrapher']
-      },
-      test_suite="tests.tests"
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.2.1-r2'
+
+install_requires = [
+    'BeautifulSoup >= 3.2.0'
+]
+
+
+setup(name='geneagrapher',
+    version=version,
+    description="Generates mathematic genealogy graph files.",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='',
+    author='David Alber',
+    author_email='alber.david@gmail.com',
+    url='http://www.davidalber.net/geneagrapher',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['ggrapher=geneagrapher.geneagrapher:ggrapher']
+    },
+    test_suite='tests.geneagrapher'
 )
