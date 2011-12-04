22be5d9e1985c1e1b1672454b1b7abf6f0db7e2b
David Alber <alber.david@gmail.com>
2011-12-03 23:39:21 -0800

    Fixing issue where tests are run twice.
    Since modifying the packaging set up, 'python setup.py test' has
    been running each test twice. This change fixes that.
    
    This closes #12.

--- tests/geneagrapher/tests.py
+++
@@ -1,5 +0,0 @@
-from test_record_methods import TestRecordMethods
-from test_node_methods import TestNodeMethods
-from test_graph_methods import TestGraphMethods
-from test_grabber_methods import TestGrabberMethods
-from test_geneagrapher_methods import TestGeneagrapherMethods
