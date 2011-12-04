8d27d5758ba25416e423a45c42dbaf1268cc9268
David Alber <alber.david@gmail.com>
2011-12-03 22:45:16 -0800

    Update the makedist.py script and remove licensify.py.
    The licensify functionality is being removed because other
    examples of open source projects that prepend every Python source
    file with the license are not known.
    
    This references #11.

--- makedist.py
+++ makedist.py
@@ -1,31 +1,9 @@
 """This tool sets up a distribution of the software by automating
-several tasks that need to be done.
-
-The directory should be in pristine condition when this is run (i.e.,
-devoid of files that need to be removed before packaging begins). It
-is best to run this on a fresh check out of the repository."""
+several tasks that need to be done."""
 
 import os
-import licensify
 
 if __name__ == '__main__':
-    # "Licensify" the source files.
-    files = ['geneagrapher/GGraph.py', 'geneagrapher/geneagrapher.py',
-             'geneagrapher/grab.py', 'geneagrapher/ggrapher.py']
-    license = 'COPYING'
-    for file in files:
-        res = licensify.prependLicense(file, license)
-        fout = open(file, "w")
-        fout.write(res)
-        fout.close()
-
-    # Remove files (including this one) that are not to be in the
-    # distribution.
-    os.system('svn rm licensify.py')
-    os.system('rm -f licensify.pyc')
-    os.system('svn rm makedist.py')
-    os.system('rm -f makedist.pyc')
-
     # Make the distribution.
     os.system('python setup.py sdist --format gztar,zip')
 
@@ -39,7 +17,3 @@ if __name__ == '__main__':
         comm = 'gpg -abs %s' % (file)
         os.system(comm)
     os.chdir('..')
-
-    # Add files to repository.
-    os.system('svn add Geneagrapher.egg-info')
-    os.system('svn add dist')
