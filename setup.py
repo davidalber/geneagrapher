6f7490db93272f9417c8d18ce0dc7f80df4d3925
David Alber <alber.david@gmail.com>
2012-01-07 23:48:57 -0800

    Change version to 1.0c2.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '1.0c1'
+version = '1.0c2'
 
 install_requires = [
     'BeautifulSoup >= 3.2.0'
