3d7cfb5801f65a01efe9526a580ea97b13b4a7b8
David Alber <alber.david@gmail.com>
2018-10-05 20:56:24 -0700

    Touch up the setup.py file

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as fin:
+    long_description = fin.read()
 
 setuptools.setup(
     name="geneagrapher",
-    version="1.0c2",
+    version="1.0",
     author="David Alber",
     author_email="alber.david@gmail.com",
     description="Mathematical genealogy grapher.",
@@ -16,10 +16,10 @@ setuptools.setup(
     install_requires=['beautifulsoup4==4.6.3', 'lxml==4.2.5'],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/davidalber/Geneagrapher",
+    url="https://github.com/davidalber/geneagrapher",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
